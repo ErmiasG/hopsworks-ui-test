b50c5b0298cb3132f1979bdb587b6ef22d793294
Ermias <ermiasg@kth.se>
2022-10-17 17:27:13 +0200

    fix navigate

--- src/test/java/io/hops/hopsworks/ui/login/LoginIT.java
+++ src/test/java/io/hops/hopsworks/ui/login/LoginIT.java
@@ -2,6 +2,8 @@ package io.hops.hopsworks.ui.login;
 
 import io.hops.hopsworks.util.WebDriverFactory;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginIT {
@@ -12,5 +14,16 @@ public class LoginIT {
     driver = WebDriverFactory.getWebDriver();
   }
   
+  @Test
+  public void gotoLogin() {
+    driver.navigate();
+  }
   
+  @Test
+  public void login() {
+    driver.navigate();
+    driver.findElement(By.name("email")).sendKeys("admin@hopsworks.ai");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.xpath("//button[text()='Login']")).submit();
+  }
 }
